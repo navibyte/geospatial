a35ff13d2ec0755c5257a9f51741f7be2697fb9b
navibyte <88932567+navispatial@users.noreply.github.com>
2022-05-24 19:29:11 +0300

    feat(geobase): put mini-library for codes back #109

---
+++ dart/geobase/lib/codes.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2020-2022 Navibyte (https://navibyte.com). All rights reserved.
+// Use of this source code is governed by a “BSD-3-Clause”-style license that is
+// specified in the LICENSE file.
+//
+// Docs: https://github.com/navibyte/geospatial
+
+/// Enums (codes) for geospatial coordinate and geometry types.
+///
+/// This libary exports a subset of `package:geobase/geobase.dart`.
+///
+/// Usage: import `package:geobase/codes.dart`
+library codes;
+
+export 'src/codes/coords.dart';
+export 'src/codes/geom.dart';
