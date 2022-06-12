ab299a42ebdf3eace54910d1f6ebc34a35e7b811
navibyte <88932567+navispatial@users.noreply.github.com>
2022-06-12 19:09:18 +0300

    feat(geobase): mini-library for constants #109

---
+++ dart/geobase/lib/constants.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2020-2022 Navibyte (https://navibyte.com). All rights reserved.
+// Use of this source code is governed by a “BSD-3-Clause”-style license that is
+// specified in the LICENSE file.
+//
+// Docs: https://github.com/navibyte/geospatial
+
+/// Geodetic and screen related constants.
+///
+/// This libary exports a subset of `package:geobase/geobase.dart`.
+///
+/// Usage: import `package:geobase/constants.dart`
+library codes;
+
+export 'src/constants/geodetic.dart';
+export 'src/constants/screen_ppi.dart';
