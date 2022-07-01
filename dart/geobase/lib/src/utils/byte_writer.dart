f09b86e45abc074e5d37ede82f63bd4f0cc12741
navibyte <88932567+navispatial@users.noreply.github.com>
2022-07-02 00:17:28 +0300

    git(geobase): byte writer util with implementation #115

--- dart/geobase/lib/src/utils/byte_writer.dart
+++ dart/geobase/lib/src/utils/byte_writer.dart
@@ -10,89 +10,167 @@ import 'dart:typed_data';
 ///
 /// The [ByteWriter] class is (at least currently) an internal utility class.
 class ByteWriter {
+  final _ChunkedByteBuffer _buffer;
+  ByteData _chunk;
+  int _offset = 0;
+
   /// Endianness to be used when writing a sequence of bytes.
   final Endian endian;
 
+  /// The buffer size used for byte chunks.
+  final int bufferSize;
+
   /// A writer (integer and floating point values) writing a sequence of bytes.
-  /// 
+  ///
   /// A writer should be buffered, but it's implementation strategies are not
   /// specified.
   ///
   /// [endian] specifies endianness to be used when writing a sequence of bytes.
-  ByteWriter.buffered({Endian endian = Endian.big}) : endian = endian;
+  ///
+  /// [bufferSize] specifies the size used for byte chunks.
+  ByteWriter.buffered({this.endian = Endian.big, this.bufferSize = 128})
+      : _buffer = _ChunkedByteBuffer(),
+        _chunk = ByteData(bufferSize);
+
+  void _reserve(int len) {
+    if (_offset + len > bufferSize) {
+      if (_flush()) {
+        _chunk = ByteData(bufferSize);
+      }
+    }
+  }
+
+  bool _flush() {
+    if (_offset > 0) {
+      _buffer.addBytes(_chunk.buffer.asUint8List(0, _offset));
+      _offset = 0;
+      return true;
+    } else {
+      _offset = 0;
+      return false;
+    }
+  }
 
   /// Collects the data written to a sequence of bytes in a Uint8List.
   Uint8List toBytes() {
-    return Uint8List(0); // todo
+     _flush();
+    return _buffer.toBytes();
   }
 
   /// Writes [value] as four bytes (IEEE 754 single-precision floating-point).
   ///
   /// See `ByteData.setFloat32` from `dart:typed_data` for reference.
   void writeFloat32(double value) {
-    // todo
+    _reserve(4);
+    _chunk.setFloat32(_offset, value, endian);
+    _offset += 4;
   }
 
   /// Writes [value] as eight bytes (IEEE 754 double-precision floating-point).
   ///
   /// See `ByteData.setFloat64` from `dart:typed_data` for reference.
   void writeFloat64(double value) {
-    // todo
+    _reserve(8);
+    _chunk.setFloat64(_offset, value, endian);
+    _offset += 8;
   }
 
   /// Writes [value] as a single byte (two's complement binary representation).
   ///
   /// See `ByteData.setInt8` from `dart:typed_data` for reference.
   void writeInt8(int value) {
-    // todo
+    _reserve(1);
+    _chunk.setInt8(_offset, value);
+    _offset += 1;
   }
 
   /// Writes [value] as two bytes (two's complement binary representation).
   ///
   /// See `ByteData.setInt16` from `dart:typed_data` for reference.
   void writeInt16(int value) {
-    // todo
+    _reserve(2);
+    _chunk.setInt16(_offset, value, endian);
+    _offset += 2;
   }
 
   /// Writes [value] as four bytes (two's complement binary representation).
   ///
   /// See `ByteData.setInt32` from `dart:typed_data` for reference.
   void writeInt32(int value) {
-    // todo
+    _reserve(4);
+    _chunk.setInt32(_offset, value, endian);
+    _offset += 4;
   }
 
   /// Writes [value] as eight bytes (two's complement binary representation).
   ///
   /// See `ByteData.setInt64` from `dart:typed_data` for reference.
   void writeInt64(int value) {
-    // todo
+    _reserve(8);
+    _chunk.setInt64(_offset, value, endian);
+    _offset += 8;
+
   }
 
   /// Writes [value] as a single byte (unsigned binary representation).
   ///
   /// See `ByteData.setUint8` from `dart:typed_data` for reference.
   void writeUint8(int value) {
-    // todo
+    _reserve(1);
+    _chunk.setUint8(_offset, value);
+    _offset += 1;
   }
 
   /// Writes [value] as two bytes (unsigned binary representation).
   ///
   /// See `ByteData.setUint16` from `dart:typed_data` for reference.
   void writeUint16(int value) {
-    // todo
+    _reserve(2);
+    _chunk.setUint16(_offset, value, endian);
+    _offset += 2;
   }
 
   /// Writes [value] as four bytes (unsigned binary representation).
   ///
   /// See `ByteData.setUint32` from `dart:typed_data` for reference.
   void writeUint32(int value) {
-    // todo
+    _reserve(4);
+    _chunk.setUint32(_offset, value, endian);
+    _offset += 4;
   }
 
   /// Writes [value] as eight bytes (unsigned binary representation).
   ///
   /// See `ByteData.setUint64` from `dart:typed_data` for reference.
   void writeUint64(int value) {
-    // todo
+    _reserve(8);
+    _chunk.setUint64(_offset, value, endian);
+    _offset += 8;
+  }
+}
+
+class _ChunkedByteBuffer {
+  final List<Uint8List> _chunks;
+  var _length = 0;
+
+  _ChunkedByteBuffer() : _chunks = <Uint8List>[];
+
+  void addBytes(Uint8List bytes) {
+    _chunks.add(bytes);
+    _length += bytes.length;
+  }
+
+  Uint8List toBytes() {
+    if (_chunks.length == 1) {
+      return _chunks.first;
+    } else {
+      final buf = Uint8List(_length);
+      var start = 0;
+      for (final chunk in _chunks) {
+        buf.setRange(start, start + chunk.length, chunk);
+        start += chunk.length;
+      }
+      return buf;
+    }
   }
 }
