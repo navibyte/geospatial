a4b4556cf0090b3f2d1132f863705523923bf837
navibyte <88932567+navispatial@users.noreply.github.com>
2023-02-18 19:23:26 +0200

    docs: update copyright texts

--- dart/geodata/lib/src/common/links/links.dart
+++ dart/geodata/lib/src/common/links/links.dart
@@ -1,8 +1,8 @@
-// Copyright (c) 2020-2021 Navibyte (https://navibyte.com). All rights reserved.
+// Copyright (c) 2020-2023 Navibyte (https://navibyte.com). All rights reserved.
 // Use of this source code is governed by a “BSD-3-Clause”-style license that is
 // specified in the LICENSE file.
 //
-// Docs: https://github.com/navibyte/dataflow
+// Docs: https://github.com/navibyte/geospatial
 
 import 'package:equatable/equatable.dart';
 
