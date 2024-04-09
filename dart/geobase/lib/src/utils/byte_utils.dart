61661701bbdffa82e1a1e762c5347ca1513a512e
navispatial <88932567+navispatial@users.noreply.github.com>
2024-04-09 09:23:53 +0300

    feat(geobase): internal utility to decode hex string

--- dart/geobase/lib/src/utils/byte_utils.dart
+++ dart/geobase/lib/src/utils/byte_utils.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2023 Navibyte (https://navibyte.com). All rights reserved.
+// Copyright (c) 2020-2024 Navibyte (https://navibyte.com). All rights reserved.
 // Use of this source code is governed by a “BSD-3-Clause”-style license that is
 // specified in the LICENSE file.
 //
@@ -13,4 +13,18 @@ import 'package:meta/meta.dart';
 extension Uint8ListUtils on Uint8List {
   /// Encode bytes as Hexadecimal (base 16) string.
   String toHex() => map((e) => e.toRadixString(16).padLeft(2, '0')).join();
+
+  /// Decode a hexadecimal (base 16) string in [data] to bytes as `Uint8List`.
+  static Uint8List fromHex(String data) {
+    final len = data.length;
+    if (len.isEven) {
+      final byteCount = len ~/ 2;
+      final bytes = List.filled(byteCount, 0);
+      for (var i = 0; i < byteCount; i++) {
+        bytes[i] = int.parse(data.substring(i * 2, i * 2 + 2), radix: 16);
+      }
+      return Uint8List.fromList(bytes);
+    }
+    throw FormatException('not valid hex string $data');
+  }
 }
