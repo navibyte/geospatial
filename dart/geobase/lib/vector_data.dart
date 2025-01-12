a6f7c821002f4093ac01fc7d22b644d2803c29ec
navispatial <88932567+navispatial@users.noreply.github.com>
2025-01-12 16:56:25 +0200

    refactor(geobase,geodata): add some exports from common to vector_data sub package too #263

--- dart/geobase/lib/vector_data.dart
+++ dart/geobase/lib/vector_data.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2023 Navibyte (https://navibyte.com). All rights reserved.
+// Copyright (c) 2020-2025 Navibyte (https://navibyte.com). All rights reserved.
 // Use of this source code is governed by a “BSD-3-Clause”-style license that is
 // specified in the LICENSE file.
 //
@@ -19,6 +19,10 @@
 /// Usage: import `package:geobase/vector_data.dart`
 library vector_data;
 
+export 'src/common/codes/coords.dart';
+export 'src/common/codes/dimensionality.dart';
+export 'src/common/codes/geom.dart';
+export 'src/common/reference/coord_ref_sys.dart';
 export 'src/vector_data/model/feature/feature.dart';
 export 'src/vector_data/model/feature/feature_builder.dart';
 export 'src/vector_data/model/feature/feature_collection.dart';
